098e292fc0ad7259fff854ceb7586b0d274dd0e6
王亓泰 <wangqitai@wangqitaideMacBook-Pro.local>
2015-12-02 19:02:46 +0800

    Initial Commit

---
+++ 01-AssembleComputer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  01-AssembleComputer
+//
+//  Created by 王亓泰 on 15/12/2.
+//  Copyright (c) 2015年 QianFeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
